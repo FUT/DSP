fc1b6ab2540aed99bbb7e664e89b57a7c8e15d9f
YuryKoleda <fut.wrk@gmail.com>
2012-10-10 00:25:50 +0300

    Discrete transformation plot
    Signed-off-by: YuryKoleda <fut.wrk@gmail.com>

--- 1/lab.rb
+++ 1/lab.rb
@@ -1,6 +1,9 @@
 require 'pry'
 require 'rubyvis'
 
+W = 1900
+H = 1000
+
 N = 32
 
 Y = Proc.new do |x|
@@ -11,41 +14,58 @@ Z = Proc.new do |x|
   Math.sin 6 * x
 end
 
-def step(n)
-  n * Math::PI * 2 / N
-end
-
-def f_full(n)
-  Y.call(step n) + Z.call(step n)
+def f_full(x)
+  Y.call(x) + Z.call(x)
 end
 
 def cx(k, f)
-  (0...N).map { |m| f.call(step m) * (Math::PI ** (-Complex::I * 2 * Math::PI * k * m / N)) }.reduce(&:+) / N
+  (0...N).map { |m| f.call(m) * (Math::PI ** (-Complex::I * 2 * Math::PI * k * m / N)) }.reduce(&:+) / N
 end
 
 def c_full(k)
-  cx(k, Y) + cx(k, Z)
+  (cx(k, Y) + cx(k, Z)).abs
 end
 
-def plot
-  data = pv.range(0, 10, 0.1).map {|x|
-    OpenStruct.new({:x=> x, :y=> Math.sin(x) + 2+rand()})
-  }
+def scale_y(data, h = H)
+  pv.Scale.linear(data, lambda {|d| d.y}).range(0, h)
 end
 
-fx = (0...N).map{ |i| OpenStruct.new({ x: step(i), y: f_full(i) }) }
-cx = (0...N).map{ |i| OpenStruct.new({ x: step(i), y: c_full(i) }) }
-
-vis = pv.Panel.new do
-  width 800
-  height 500
-  line do
-    data fx
-    line_width 5
-    stroke_style 'red'
-  end
+def scale_x(data, w = W)
+  pv.Scale.linear(data, lambda {|d| d.x}).range(0, w)
 end
+
+fx = pv.range(-30, 30, 0.07).map {|x| OpenStruct.new({ x: x, y: f_full(x) }) }
+sfx = scale_x fx
+sfy = scale_y fx
+
+cx = pv.range(-30, 30, 0.07).map {|x| OpenStruct.new({ x: x, y: c_full(x) }) }
+scx = scale_x cx
+scy = scale_y cx, H/2
+
+vis = pv.Panel.new()
+  .width(W)
+  .height(H)
+  .fillStyle('white')
+
+vis.add(pv.Rule)
+  .line_width(3)
+
+vis.add(pv.Rule)
+  .line_width(3)
+  .left(W/2)
+
+vis.add(pv.Line)
+  .data(fx)
+  .line_width(1)
+  .left(->(d) {sfx.scale(d.x)})
+  .bottom(->(d) {sfy.scale(d.y)})
+
+vis.add(pv.Line)
+  .data(cx)
+  .line_width(2)
+  .left(->(d) {scx.scale(d.x)})
+  .bottom(->(d) {scy.scale(d.y) + H/2})
+  .strokeStyle('red')
 
 vis.render()
-p vis.to_svg
 File.open('fourier.svg', 'w') { |file| file << vis.to_svg }
