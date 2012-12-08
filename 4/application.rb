0e0aa59982028c9d20810003320b804e284edaaa
Yury Kaliada <fut.wrk@gmail.com>
2012-12-08 11:52:43 +0300

    Black & white image. Hist fixed
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- 4/application.rb
+++ 4/application.rb
@@ -2,6 +2,7 @@ require 'pry'
 require 'RMagick'
 
 img = Magick::Image.read('image.png').first
+img.write('image.png')
 
 def draw_hist(img, w=900, h=500, file='histogram.png')
   canvas = Magick::ImageList.new
@@ -9,8 +10,7 @@ def draw_hist(img, w=900, h=500, file='histogram.png')
 
   hist_data = img.quantize.color_histogram.to_a.sort { |a, b| a[1] <=> b[1] }
   hist_max = hist_data.map{ |d| d[1] }.max
-  hist_step = 1.0 * w / hist_data.size
-  hist_x = 0
+  hist_step = 1.0 * w / hist_data.size - 1
 
   draw = Magick::Draw.new
   draw.stroke_width(hist_step)
@@ -18,8 +18,8 @@ def draw_hist(img, w=900, h=500, file='histogram.png')
   hist_data.each do |pixel, count|
     height = 1.0 * count / hist_max * h
     draw.stroke pixel.to_color
+    hist_x = 1.0 * pixel.intensity / 65535 * w
     draw.line(hist_x, h - height, hist_x, h)
-    hist_x += hist_step
   end
 
   draw.draw(canvas)
