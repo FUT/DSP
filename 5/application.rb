e8b6424c13b8de45a78a18b00ef0c5f1b3013d88
Yury Kaliada <fut.wrk@gmail.com>
2013-02-13 08:32:11 +0300

    Image marking was implemented
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- 5/application.rb
+++ 5/application.rb
@@ -1,19 +1,60 @@
 require 'pry'
 require 'RMagick'
 
-# img = Magick::Image.read('image.jpg').first
-# img.write 'result1.jpg'
-# (med = img.median_filter(2)).write 'median.jpg'
-# (bin = med.threshold(Magick::MaxRGB * 0.75)).write 'binarized.jpg'
-#
-# result = Magick::Image.read('result1.jpg').first
-# result.each_pixel do |p, c, r|
-#   p "row #{r}" if c == 0
-#   square = ([-1, 0, 1] * 2).permutation(2).to_a.uniq
-#   in_group = square.all? { |dx, dy| bin.pixel_color(c + dx, r + dy).intensity < 100 }
-#   result.pixel_color(c, r, in_group ? 'red' : 'white')
+SQUARE = [[-1, 0], [-1, 1], [-1, -1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+
+img = Magick::Image.read('image.jpg').first
+img.write 'result1.jpg'
+(med = img.median_filter(2)).write 'median.jpg'
+(@bin = med.threshold(Magick::MaxRGB * 0.75)).write 'binarized.jpg'
+
+@result = Magick::Image.read('result1.jpg').first
+@result.each_pixel { |p, c, r| @result.pixel_color(c,r, 'white') }
+$checked = {}
+$stack = []
+
+# def color_byte
+#   '%02X' % rand(155)
 # end
-# result.write 'result.jpg'
+#
+# def new_color
+#   "##{color_byte}#{color_byte}#{color_byte}"
+# end
+# $color = new_color
+#
+# def in_group?(c, r)
+#   SQUARE.all? { |dx, dy| @bin.pixel_color(c + dx, r + dy).intensity < 100 }
+# end
+#
+# def group(c, r, color = new_color)
+#   p "#{c} #{r}"
+#   if in_group?(c, r)
+#     $stack << [c, r]
+#     while !$stack.empty?
+#       sc, sr = $stack.shift
+#       p "- #{sc} #{sr}              len #{$stack.length}"
+#       $checked[sr] ||= []
+#       next if $checked[sr].include?(sc)
+#       $checked[sr] << sc
+#       next unless in_group?(sc, sr)
+#       @result.pixel_color(sc, sr, color)
+#       SQUARE.each { |dx, dy| $stack << [sc + dx, sr + dy] }
+#     end
+#     $color = new_color
+#   else
+#     $checked[r] ||= []
+#     $checked[r] << c
+#   end
+# end
+#
+# @result.each_pixel do |p, c, r|
+#   next unless @result.pixel_color(c,r).intensity > 60000
+#   p "row #{r}" if c == 0
+#   group(c,r)
+#   $checked[r] ||= []
+#   $checked[r] << c
+# end
+# @result.write 'result.jpg'
 
 def show(file)
   @buttons_x ||= 0
